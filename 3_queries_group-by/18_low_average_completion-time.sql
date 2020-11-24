584988dd0eb2d849e0c69df9512ce1aae12b88f4
Luca Properzi <abramoproperzi@gmail.com>
2020-11-24 21:07:25 +0000

    add two new tables along with fake data - teachers and assistance requests

--- 3_queries_group-by/18_low_average_completion-time.sql
+++ 3_queries_group-by/18_low_average_completion-time.sql
@@ -1,8 +1,10 @@
-SELECT students.name AS student, 
-  AVG(assignment_submissions.duration) AS average_assignment_duration, 
-  AVG(assignments.duration) AS average_estimated_duration
+SELECT students.name as student, 
+  avg(assignment_submissions.duration) as average_assignment_duration, 
+  avg(assignments.duration) as average_estimated_duration
 FROM students
-WHERE students.end_date IS NULL
-JOIN assignment_submissions on assignment_submissions.id = assignment_id
-JOIN assignments on assignments.id = assignments_id
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignments.id = assignment_id
+WHERE end_date IS NULL
 GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
